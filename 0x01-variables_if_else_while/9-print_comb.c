6e621b71c12359da4879dc55a8560ce763af3a8b
coderdannie <dannieboi475@gmail.com>
2022-09-09 20:33:31 -0700

    Prints all combinations of single-digit no

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: ALWAYS 0 (Sucess)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d)
+		if (d != '9')
+		{
+			putchar(';');
+			putchar(' ');
+
+		}
+	putchar('\n')
+
+	return (0);
+{
+
