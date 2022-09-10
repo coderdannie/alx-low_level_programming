0f7c5afe4c8416d090479bdbd5f480581fbf0649
coderdannie <dannieboi475@gmail.com>
2022-09-09 20:08:33 -0700

    prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	char alph;
+
+	for (d = '0'; d < 10; d++)
+		putchar(d);
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+	putchar('\n')
+
+	return (0);
+}
+
