17e33a57564476d8ff9e3b5c41b6b4381e43aa42
coderdannie <dannieboi475@gmail.com>
2022-09-09 19:22:21 -0700

    print all single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+
+}
