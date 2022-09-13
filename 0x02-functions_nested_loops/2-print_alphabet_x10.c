4222991838d58bb6e1bf1cccba09f33b651aeef2
coderdannie <dannieboi475@gmail.com>
2022-09-13 11:21:46 -0700

    print letter ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+
+	char second_letters
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (second_letters = 'a'; second_letters <= 'z'; second_letters++)
+		{
+			_putchar(second_letters);
+		}
+		_putchar('\n');
+	}
+}
