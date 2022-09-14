476c305c841cb7cc3087695d0617fb4a04cdd641
coderdannie <dannieboi475@gmail.com>
2022-09-13 18:29:14 -0700

    Prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table of an integer starting with  zero
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i, y, mul;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+
+			mul = i * y;
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (y == 0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else if ((mul < 10) && (y != 0))
+			{
+				_putchar(' ');
+				_putchar((mul % 10) + '0');
+			}
+		{
+		_putchar('\n');
+	}
+}
