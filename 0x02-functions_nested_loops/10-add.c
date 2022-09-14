02b2717392d3ed516349b93741b94b2bb1877d76
coderdannie <dannieboi475@gmail.com>
2022-09-13 18:59:58 -0700

    Adds two unknown ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Adds two integers and returns the result
+ * @x: number been added
+ * @y: number been added
+ * Return: Always 0.
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
