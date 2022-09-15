8171abfa1da059310a0c3ba01c301fee0c4fb12e
coderdannie <dannieboi475@gmail.com>
2022-09-15 12:32:25 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -17,9 +18,9 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("%s " fb);
+			printf("%s ", fb);
 		else if (i == 100)
-			printf("%s ", b);
+			printf("%s", b);
 		else if (i % 3 == 0)
 			printf("%s ", f);
 		else if (i % 5 == 0)
