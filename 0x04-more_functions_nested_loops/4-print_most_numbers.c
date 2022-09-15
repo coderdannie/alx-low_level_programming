112ecaaaa0d6e55dd34d5caf5226a618d02017cc
coderdannie <dannieboi475@gmail.com>
2022-09-15 09:32:57 -0700

    Prints numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - a function that prints the numbers, 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if ((num == '2') || (num == '4'))
+		{
+			continue;
+		{
+		_putchar(num)
+	}
+	_putchar('\n')
+}
+
