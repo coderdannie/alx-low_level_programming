14c2a47898fcd4655487902ef45b2f3774891b4d
coderdannie <dannieboi475@gmail.com>
2022-09-15 11:33:34 -0700

    Print square pattern

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,13 @@ void print_square(int size)
 	int i, j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (i = 0; i <= size; i++)
 		{
-			_putchar('#');
-			for (j = 2; j <= size; j++)
-			{
+			for (j = 0; j <= size; j++)
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
